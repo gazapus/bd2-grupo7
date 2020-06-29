5027cb8c4cdd582d98540d840ce91b9d91908328
Cristian Villafane <cristianv129@hotmail.com>
2020-06-29 18:38:51 -0300

    update test_consultas.js

--- test_consultas.js
+++ test_consultas.js
@@ -6,6 +6,6 @@ rankingDeProductosPorMonto(new Date(2020, 1, 1), new Date(), 1); // sucursal 1
 
 // Test consulta 7
 rankingDeClientesPorMonto(new Date(2020, 1) , new Date());    // cadena completa
-rankingDeClientesPorMonto(new Date(2020, 1, 1) , new Date(), 1);    // cadena completa
-//rankingDeClientesPorMonto(new Date(2020, 1, 1) , new Date(), 2);    // cadena completa
-//rankingDeClientesPorMonto(new Date(2020, 1, 1) , new Date(), 3);    // cadena completa
+rankingDeClientesPorMonto(new Date(2020, 1, 1) , new Date(), 1);    // sucursal 1
+//rankingDeClientesPorMonto(new Date(2020, 1, 1) , new Date(), 2);    // sucursal 2
+//rankingDeClientesPorMonto(new Date(2020, 1, 1) , new Date(), 3);    // sucursal 3
